fdd40cddbc10d3a9ccf2322053c2dc6bbd602328
Talgat.Lukmanov <Talgat.Lukmanov@tatar.ru>
2020-04-02 09:06:27 +0300

    изменение 1 в ветке2

--- study/study/Program.cs
+++ study/study/Program.cs
@@ -6,7 +6,7 @@ namespace study
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет Мир!");
         }
     }
 }
